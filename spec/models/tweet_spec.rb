6178254ebcf23f5d821c13e9db522ae3803b67fb
Felipe <fchiapini@gmail.com>
2016-02-15 16:30:21 -0200

    Added shoulda matchers for specs

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -11,4 +11,11 @@ describe Tweet do
   it { should respond_to( :twitter_id ) }
 
   it { should be_valid }
+
+  it { should validate_presence_of( :user ) }
+  it { should validate_presence_of( :text ) }
+  it { should validate_presence_of( :posted_at ) }
+  it { should validate_presence_of( :twitter_id ) }
+  it { should validate_uniqueness_of( :twitter_id ) }
+
 end

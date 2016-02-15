c26a455f83706f78554662c9c2cf336fdb0f4e7c
Felipe <fchiapini@gmail.com>
2016-02-15 14:25:15 -0200

    Set the routes constraints for the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 RailsTwitterApi::Application.routes.draw do
-  
+  # Api definition
+  namespace :api, defaults: { format: :json },
+                              constraints: { subdomain: 'api' }, path: '/' do
+    # We are going to list our resources here
+  end
 end
